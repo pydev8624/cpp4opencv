b408551de70ca4023829aff4a3eb8483ed10962f
.asm <irajmirzazadeh2024@gmail.com>
2025-04-02 00:30:21 +0330

    Create code3.cpp

---
+++ code3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+    cout<<"a ?"<<endl;
+    cin>>a;
+    cout<<"b?"<<endl;
+    cin>>b;
+    cout<<"a+b="<<a+b<<endl;
+   return 0;
+}
