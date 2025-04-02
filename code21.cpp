c1067c3c7a9dcaa336a3b2ffca0553118c44c627
.asm <irajmirzazadeh2024@gmail.com>
2025-04-02 22:40:52 +0330

    Create code21.cpp

---
+++ code21.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int c=0;
+    c++;
+    cout<<c<<endl;
+    c--;
+    cout<<c<<endl;
+    cout<<c++<<endl;
+    cout<<c<<endl;
+    cout<<c--<<endl;
+    cout<<c<<endl;
+    return 0;
+}
