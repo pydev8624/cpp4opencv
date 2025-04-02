388662e14bc998e564cac2dfe1aaa6ffb94b02d2
.asm <irajmirzazadeh2024@gmail.com>
2025-04-02 22:23:05 +0330

    Create code17.cpp

---
+++ code17.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a=1;
+    int b=2;
+    cout<<a+b<<endl;
+    cout<<a-b<<endl;
+    cout<<a*b<<endl;
+    cout<<a/b<<endl;
+    cout<<(float)(a)/(float)(b)<<endl;
+    cout<<a%b<<endl;
+    return 0;
+}
