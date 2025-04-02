a1cb05610fe65ab919729b75761778aac178123b
.asm <irajmirzazadeh2024@gmail.com>
2025-04-02 22:36:47 +0330

    Create code20.cpp

---
+++ code20.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int x=0;
+    x=1;
+    cout<<x<<endl;
+    x+=1;
+    cout<<x<<endl;
+    x-=5;
+    cout<<x<<endl;
+    x*=6;
+    cout<<x<<endl;
+    x/=7;
+    cout<<x<<endl;
+    x%=2;
+    cout<<x<<endl;
+    float f=35.26;
+    f/=10;
+    cout<<f<<endl;
+    return 0;
+}
