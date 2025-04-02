aa2baffdacce4ed53bfa3ff99ec505870e3c791d
.asm <irajmirzazadeh2024@gmail.com>
2025-04-02 04:09:19 +0330

    Create code7.cpp

---
+++ code7.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main() {
+   string name ,family;
+   cout<<"name : ";
+   cin>>name;
+   cout<<"family : ";
+   cin>>family;
+   cout<<name<<" "<<family<<endl;
+   return 0;
+}
