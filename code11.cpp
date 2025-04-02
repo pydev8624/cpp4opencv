7dc2a486136d296a33975d0d2bdc05839a37ff14
.asm <irajmirzazadeh2024@gmail.com>
2025-04-02 06:39:18 +0330

    Create code11.cpp

---
+++ code11.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main() {
+   string text="C++ is object oriented :)";
+   cout<<text[0]<<endl;
+   cout<<text[5]<<endl;
+   cout<<text[text.length()-1]<<endl;
+   return 0;
+}
