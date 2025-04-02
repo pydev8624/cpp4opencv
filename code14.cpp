41601cf0040f2b1b7af7635f8adeb870a72bdf87
.asm <irajmirzazadeh2024@gmail.com>
2025-04-02 07:16:18 +0330

    Create code14.cpp

---
+++ code14.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main() {
+   string text1="C++ is awesome and programmers like it";
+   cout<<text1.substr(3,9)<<endl;
+   return 0;
+}
