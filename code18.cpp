8e8071990ed009d937fd8ac0474d8ba0d8e9060b
.asm <irajmirzazadeh2024@gmail.com>
2025-04-02 22:29:17 +0330

    Create code18.cpp

---
+++ code18.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    bool a,b;
+    a=true ;
+    b=false;
+    cout<<(a&&b)<<endl;
+    cout<<(a||b)<<endl;
+    cout<<(!a)<<endl;
+    return 0;
+}
