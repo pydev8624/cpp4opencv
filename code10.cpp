83ce767dd3284b5993b9fe68ab91e29d9892e0b1
.asm <irajmirzazadeh2024@gmail.com>
2025-04-02 06:09:58 +0330

    Create code10.cpp

---
+++ code10.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main() {
+   string text1="C++ ";
+   string text2="is OOP";
+   string text3=text1.append(text2);
+   cout<<text1<<endl;
+   cout<<text2<<endl;
+   cout<<text3<<endl;
+   return 0;
+}
