f5303fdd9dbd0b045f160d94d79b90737c3deeae
.asm <irajmirzazadeh2024@gmail.com>
2025-04-04 05:20:11 +0330

    Create code23.cpp

---
+++ code23.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int c=0;
+    cout<<"done1"<<endl;
+    if (c>0)
+    {
+        cout<<"done2"<<endl;
+    }
+    else
+    {
+        cout<<"<=0"<<endl;
+    }
+    cout<<"done3"<<endl;
+    return 0;
+}
