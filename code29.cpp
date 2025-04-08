4bceb8adef13f4216debdd532b0c4af912a4b028
.asm <irajmirzazadeh2024@gmail.com>
2025-04-08 18:35:14 +0330

    Create code29.cpp

---
+++ code29.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a=10;
+    int *p=&a;
+    cout<<a<<endl;
+    cout<<*p<<endl;
+    cout<<&a<<endl;
+    cout<<p<<endl;
+    ++p ;
+    cout<<a<<endl;
+    cout<<*p<<endl;
+    cout<<&a<<endl;
+    cout<<p<<endl;
+}
