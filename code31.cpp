83d3546313c324359e2dc09c4323eab3d6832722
.asm <irajmirzazadeh2024@gmail.com>
2025-04-10 03:56:11 +0330

    Create code31.cpp

---
+++ code31.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num[3];
+    int i;
+    for (i=0;i<3;i++)
+    {
+        cin>>num[i];
+    }
+    for (i=0;i<3;i++)
+    {
+        cout<<num[i]<<endl;
+    }
+}
