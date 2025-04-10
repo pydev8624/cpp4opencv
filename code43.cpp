92da41f255cb98048432f312021cd5a432eb94cf
.asm <irajmirzazadeh2024@gmail.com>
2025-04-10 16:55:08 +0330

    Create code43.cpp

---
+++ code43.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include<vector>
+
+using namespace std;
+
+int main()
+{
+  vector<string> name={"python","C","C++","java"};
+  cout<<name.front()<<endl;
+  cout<<name.back()<<endl;
+}
