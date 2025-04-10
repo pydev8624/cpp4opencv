121f772350c84114f291299d392ade5d60eda215
.asm <irajmirzazadeh2024@gmail.com>
2025-04-10 04:10:14 +0330

    Create code32.cpp

---
+++ code32.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string name[]={"python","assembly","c++"};
+    for (int i=0;i<3;i++)
+    {
+        cout<<name[i]<<endl;
+    }
+}
