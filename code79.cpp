dcaea46f55af891b9d05ad7005eccd7b876d81d4
.asm <irajmirzazadeh2024@gmail.com>
2025-04-21 22:14:35 +0330

    Create code79.cpp

---
+++ code79.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+class Animal {
+public:
+    virtual void sound() {
+        cout << "Animal makes a sound" << endl;
+    }
+};
+
+class Dog : public Animal {
+public:
+    void sound() override {
+        cout << "Dog barks" << endl;
+    }
+};
+
+int main() {
+    Animal* animal1;
+
+    Dog dog;
+    
+    animal1 = &dog;
+    animal1->sound();
+
+    return 0;
+}
