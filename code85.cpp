d971c5e1dac334c82438a0fe1d5bec0b9c9d9654
.asm <irajmirzazadeh2024@gmail.com>
2025-05-06 20:08:57 +0330

    Create code85.cpp

---
+++ code85.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+int main() {
+    list<int> myList = {10, 20, 30};
+    myList.pop_front();
+
+    for (int x : myList)
+        cout << x << " ";
+    return 0;
+}
