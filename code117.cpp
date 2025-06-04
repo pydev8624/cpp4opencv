592a9814e8844e6f082a7bfa8837ba296f9b3bff
.asm <irajmirzazadeh2024@gmail.com>
2025-06-05 01:56:53 +0330

    Create code117.cpp

---
+++ code117.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main() {
+    map<int, string> names;
+    names.insert({1, "ali"});
+    names.insert({2, "amie"});
+    names.insert({3, "david"}); 
+    names.insert({4, "akbar"}); 
+
+    for (auto item:names){
+        cout<<item.first<<" "<<item.second<<endl;
+    }
+
+}
