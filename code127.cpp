e0c1599c2d7ebe71e08bafb234ace686f7e0a47d
.asm <irajmirzazadeh2024@gmail.com>
2025-06-08 23:58:13 +0330

    Create code127.cpp

---
+++ code127.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+ int age=20;
+ float grade=89.7;
+ string name="David";
+ bool live=true;
+
+ cout<<"age : "<<age<<endl<<"grade : "<<grade<<endl;
+ cout<<"name : "<<name<<endl<<"live : "<<live<<endl;
+}
