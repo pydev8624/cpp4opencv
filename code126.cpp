711f99dc663cb55e64c83d5fb255d5c03ae140fd
.asm <irajmirzazadeh2024@gmail.com>
2025-06-08 23:53:37 +0330

    Create code126.cpp

---
+++ code126.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+ string name,family;
+ cout<<"name : "<<endl;
+ getline(cin,name);
+ cout<<"family : "<<endl;
+ getline(cin,family);
+ cout<<name<<" "<<family<<endl;
+}
