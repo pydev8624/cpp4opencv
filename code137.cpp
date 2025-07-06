49c954a3aeee7c75f0838349598db175270e892f
.asm <irajmirzazadeh2024@gmail.com>
2025-07-06 16:02:15 +0330

    Create code137.cpp

---
+++ code137.cpp
@@ -0,0 +1,14 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace cv;
+
+int main() {
+    
+    Point p1(10,10);
+    Point2f p2(9.5,8.7);
+    Point2d p3(11.2564821532, 22.98520005);
+    
+    waitKey(0);
+    return 0;
+}
